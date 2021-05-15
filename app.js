70328b3f52865b50f306d8e178d5bd0fe807c93c
AngieDutra <angiedutraa@gmail.com>
2021-05-15 17:19:55 -0300

    Agregue en app.js los gets para todos los html hasta el momento. Termine los html de home y productCart.

--- app.js
+++ app.js
@@ -5,5 +5,8 @@ const path = require('path');
 app.listen(3000, ()=>console.log('El servidor esta corriendo en http://localhost:3000'));
 app.use(express.static(path.resolve(__dirname, './public')));
 
-app.get('/',(req, res)=>{
-    res.sendFile(path.resolve(__dirname, './views/home.html'))});
+app.get('/',(req, res)=>{res.sendFile(path.resolve(__dirname, './views/home.html'))});
+app.get('/login', (req, res) => {res.sendFile(path.resolve(__dirname, './views/login.html'))})
+app.get('/productCart', (req, res) => {res.sendFile(path.resolve(__dirname, './views/productCart.html'))})
+app.get('/productDetail', (req, res) => {res.sendFile(path.resolve(__dirname, './views/productDetail.html'))})
+app.get('/register', (req, res) => {res.sendFile(path.resolve(__dirname, './views/register.html'))})
