830d2f42a8311855236d46fd2727e4c38cda6eac
AngieDutra <angiedutraa@gmail.com>
2021-05-15 18:17:25 -0300

    Agregue mas etiquetas, simbolos y comentarios para organizar el codigo de los html home y productCart.

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.listen(3000, ()=>console.log('El servidor esta corriendo en http://localhost
 app.use(express.static(path.resolve(__dirname, './public')));
 
 app.get('/',(req, res)=>{res.sendFile(path.resolve(__dirname, './views/home.html'))});
-app.get('/login', (req, res) => {res.sendFile(path.resolve(__dirname, './views/login.html'))})
-app.get('/productCart', (req, res) => {res.sendFile(path.resolve(__dirname, './views/productCart.html'))})
-app.get('/productDetail', (req, res) => {res.sendFile(path.resolve(__dirname, './views/productDetail.html'))})
-app.get('/register', (req, res) => {res.sendFile(path.resolve(__dirname, './views/register.html'))})
+app.get('/login', (req, res) => {res.sendFile(path.resolve(__dirname, './views/login.html'))});
+app.get('/productCart', (req, res) => {res.sendFile(path.resolve(__dirname, './views/productCart.html'))});
+app.get('/productDetail', (req, res) => {res.sendFile(path.resolve(__dirname, './views/productDetail.html'))});
+app.get('/register', (req, res) => {res.sendFile(path.resolve(__dirname, './views/register.html'))});
