c19811ef6bc4e2446a66184c0192b91aed44f037
AngieDutra <angiedutraa@gmail.com>
2021-05-22 19:38:39 -0300

    Agregue el index.html

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require('path');
 app.listen(3000, ()=>console.log('El servidor esta corriendo en http://localhost:3000'));
 app.use(express.static(path.resolve(__dirname, './public')));
 
-app.get('/',(req, res)=>{res.sendFile(path.resolve(__dirname, './views/home.html'))});
+app.get('/',(req, res)=>{res.sendFile(path.resolve(__dirname, './views/index.html'))});
 app.get('/login', (req, res) => {res.sendFile(path.resolve(__dirname, './views/login.html'))});
 app.get('/productCart', (req, res) => {res.sendFile(path.resolve(__dirname, './views/productCart.html'))});
 app.get('/productDetail', (req, res) => {res.sendFile(path.resolve(__dirname, './views/productDetail.html'))});
