a68f6c369150384cf687fcc82140be81996cbdc8
AngieDutra <angiedutraa@gmail.com>
2021-06-10 19:59:33 -0300

    Updates

--- src/app.js
+++ src/app.js
@@ -2,8 +2,19 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.listen(3000, ()=>console.log('El servidor esta corriendo en http://localhost:3000'));
+// Servidor
+app.set("port", process.env.PORT || 3000);
+app.listen(app.get('port'), ()=> console.log('Server en http://localhost:' + app.get('port')));
+
+// Configuración
 app.use(express.static(path.resolve(__dirname, './public')));
+
+//View Engine
+app.set("view engine", "ejs");
+app.set("views", path.resolve(__dirname, "./views"));
+
+//Rutas
+
 
 app.get('/',(req, res)=>{res.sendFile(path.resolve(__dirname, './views/index.html'))});
 app.get('/login', (req, res) => {res.sendFile(path.resolve(__dirname, './views/login.html'))});
