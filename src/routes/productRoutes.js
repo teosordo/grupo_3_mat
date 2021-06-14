38d0bdd42a1ff5bdb022c96022a6d633a0acd7ff
AngieDutra <angiedutraa@gmail.com>
2021-06-14 20:28:40 -0300

    productDetail dinamico y updates de estilo

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const productController = require('../controllers/productController');
 
 // Product Detail
-router.get('/productDetail', productController.productDetail);
+//router.get('/productDetail', productController.productDetail);
+router.get('/productDetail/:id', productController.productDetail);
 
 module.exports = router;
