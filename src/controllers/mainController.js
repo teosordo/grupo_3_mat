ead49ea79d66de61cb5db0d76633bae0b0b57bdd
matiasc289 <matiascelentano562@gmail.com>
2021-06-14 21:10:50 -0300

    index dinamico

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,7 +1,11 @@
+const product = [
+    { nombre: "Memoria RAM", marca:"X", precio: "$$", envio: "Gratis!", disponibilidad: "fas fa-check", pago: "fas fa-credit-card", garantia: "1 año", img:"/imgs/index/componentRAM.jpg", caract: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, nemo distinctio fugiat repellendus rem, deserunt nihil nisi quis repellat et numquam placeat. Voluptates, porro unde. Iusto nesciunt praesentium expedita mollitia.", esp: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, nemo distinctio fugiat repellendus rem, deserunt nihil nisi quis repellat et numquam placeat. Voluptates, porro unde. Iusto nesciunt praesentium expedita mollitia."},
+    { nombre: "Ordenador", marca:"Y", precio: "$$$", envio: "Gratis!", disponibilidad: "fas fa-check", pago: "fas fa-credit-card", garantia: "2 años", img:"/imgs/index/componentCASE.jpg", caract: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, nemo distinctio fugiat repellendus rem, deserunt nihil nisi quis repellat et numquam placeat. Voluptates, porro unde. Iusto nesciunt praesentium expedita mollitia.", esp: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, nemo distinctio fugiat repellendus rem, deserunt nihil nisi quis repellat et numquam placeat. Voluptates, porro unde. Iusto nesciunt praesentium expedita mollitia."},
+    { nombre: "Tarjeta gráfica", marca:"Z", precio: "$$$", envio: "Gratis!", disponibilidad: "fas fa-check", pago: "fas fa-credit-card", garantia: "6 meses", img:"/imgs/index/componentGC.jpg", caract: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, nemo distinctio fugiat repellendus rem, deserunt nihil nisi quis repellat et numquam placeat. Voluptates, porro unde. Iusto nesciunt praesentium expedita mollitia.", esp: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, nemo distinctio fugiat repellendus rem, deserunt nihil nisi quis repellat et numquam placeat. Voluptates, porro unde. Iusto nesciunt praesentium expedita mollitia."}
+];
 const mainController = {
     index: (req, res) => {
-        res.render('index');
+        res.render('index', {product: product});
     }
 };
-
 module.exports = mainController;
