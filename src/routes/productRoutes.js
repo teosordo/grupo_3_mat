c10d0a855c30b77d25799c0161b454017195d2e8
matiasc289 <matiascelentano562@gmail.com>
2021-06-18 19:30:35 -0300

    form crear y editar

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -5,5 +5,6 @@ const productController = require('../controllers/productController');
 // Product Detail
 //router.get('/productDetail', productController.productDetail);
 router.get('/productDetail/:id', productController.productDetail);
-
+router.get('/product/create', productController.productNewProduct);
+router.get('/product/edit/:id', productController.productEdit);
 module.exports = router;
