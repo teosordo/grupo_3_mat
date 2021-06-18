83e106a8f0eac3c436d24f5ac94498ecfbf1afca
AngieDutra <angiedutraa@gmail.com>
2021-06-18 19:57:19 -0300

    Updates

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -3,10 +3,17 @@ const product = [
     { nombre: "Ordenador", marca:"Y", precio: "$$$", envio: "Gratis!", disponibilidad: "fas fa-check", pago: "fas fa-credit-card", garantia: "2 años", img:"/imgs/index/componentCASE.jpg", caract: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, nemo distinctio fugiat repellendus rem, deserunt nihil nisi quis repellat et numquam placeat. Voluptates, porro unde. Iusto nesciunt praesentium expedita mollitia.", esp: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, nemo distinctio fugiat repellendus rem, deserunt nihil nisi quis repellat et numquam placeat. Voluptates, porro unde. Iusto nesciunt praesentium expedita mollitia."},
     { nombre: "Tarjeta gráfica", marca:"Z", precio: "$$$", envio: "Gratis!", disponibilidad: "fas fa-check", pago: "fas fa-credit-card", garantia: "6 meses", img:"/imgs/index/componentGC.jpg", caract: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, nemo distinctio fugiat repellendus rem, deserunt nihil nisi quis repellat et numquam placeat. Voluptates, porro unde. Iusto nesciunt praesentium expedita mollitia.", esp: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, nemo distinctio fugiat repellendus rem, deserunt nihil nisi quis repellat et numquam placeat. Voluptates, porro unde. Iusto nesciunt praesentium expedita mollitia."}
 ];
+// Usar el js de Matias
+/*
+const fs = require('fs');
+const path = require('path');
 
+const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+*/
 const productController = {
     productDetail: (req, res) => {
-        idProduct = req.params.id;
+        const idProduct = req.params.id;
         res.render('productDetail', {product: product});
     }
 };
