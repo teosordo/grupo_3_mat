37ec2a7a3b5f849988e219896d8db5581a016290
matiasc289 <matiascelentano562@gmail.com>
2021-06-21 23:47:31 -0300

    productDetail y Edit ahora redireccionan a home si no se encuentra el id

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,31 +1,31 @@
+const { response } = require('express');
 const productsFunctions = require('../models/product');
 //const products = productsFunctions.all()
 const productController = {
     productDetail: (req, res) => {
         let idProduct = req.params.id
         if(idProduct == undefined || idProduct == 0){
-            res.redirect('/')
+            res.redirect('/');
         }else{
-            let product = productsFunctions.search(idProduct)
-            res.render('productDetail', {product: product[0]})
+            let product = productsFunctions.search(idProduct);
+            return product == false ?  res.redirect('/'):res.render('productDetail',{product: product[0]})
         }
     },
     productNewProduct:(req,res) => {
         res.render('newProductCreate')
     },
     productCreate:(req,res)=>{
-        let result = productsFunctions.create(req.body, req.file)
-        return result == true ? res.redirect('/') : res.send("ERROR")
+        let result = productsFunctions.create(req.body, req.file);
+        return result == true ? res.redirect('/') : res.send("ERROR");
     },
     productEdit:(req,res) => {
-        let idProduct = req.params.id
+        let idProduct = req.params.id;
         if(idProduct == undefined || idProduct == 0){
             res.redirect('/')
         }else{
-            let product = productsFunctions.search(idProduct)
-            res.render('newProductEdit', {product: product[0]})
+            let product = productsFunctions.search(idProduct);
+            return product == false ? res.redirect('/') : res.render('newProductEdit',{product: product[0]})
         }
-        
     }
 };
 
