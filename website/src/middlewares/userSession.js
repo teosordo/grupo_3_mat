eaa8056e2b89c613ec4108156ecce8e9a56a4eee
AngieDutra <angiedutraa@gmail.com>
2021-07-26 19:11:00 -0300

    Updates

--- website/src/middlewares/userSession.js
+++ website/src/middlewares/userSession.js
@@ -1,7 +1,7 @@
 const user = require("../models/user");
 
 module.exports = (req, res, next) => {
-    let userLogin = req.session.userLogged /*|| user.findByField(req.cookies.email);*/
+    let userLogin = req.session.userLogged /*|| user.findByField(req.cookies.email) Le falta un parámetro*/;
     res.locals.user = userLogin ? userLogin : null;
 
     let userLog = req.session.logged
