a17bd9175f3a6bcbba6a4549255c8bd345c4d211
matiasc289 <matiascelentano562@gmail.com>
2021-08-10 17:16:37 -0300

    user register update a db

---
+++ website/src/database/models/User.js
@@ -0,0 +1,43 @@
+module.exports = (sequelize, dataTypes) => {
+    let alias = 'User'
+    let cols = {
+        id: {
+            type: dataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+          },
+          firstName: {
+            type: dataTypes.STRING,
+            allowNull: false
+          },
+          lastName: {
+            type: dataTypes.STRING,
+            allowNull: false
+          },
+          username: {
+            type: dataTypes.STRING,
+            allowNull: false,
+            unique: true
+          },
+          email: {
+            type: dataTypes.STRING,
+            allowNull: false,
+            unique: true
+          },
+          password: {
+            type: dataTypes.STRING,
+            allowNull: false
+          },
+          avatar: {
+            type: dataTypes.STRING,
+            allowNull: false
+          }
+    };
+    let config = {
+        timestamps: false,
+        deletedAt: false,
+    }
+    const User = sequelize.define(alias,cols,config)
+    
+    return User
+}
