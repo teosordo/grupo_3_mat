b71b5b92d103a2e4cb688f4a6845818cafe658cd
matiasc289 <matiascelentano562@gmail.com>
2021-08-13 22:36:32 -0300

    update login con db

--- website/src/middlewares/users/loginValidator.js
+++ website/src/middlewares/users/loginValidator.js
@@ -1,25 +1,29 @@
 const {body} = require('express-validator');
-const {findByField} = require('../../models/user');
+const db = require('../../database/models')
 const bcrypt = require('bcryptjs');
 
 module.exports = [
     body('email')
         .notEmpty().withMessage('Ingrese su e-mail').bail()
         .isEmail().withMessage('Ingrese un e-mail válido').bail()
-        .custom(value=>{
-            let userToLogin = findByField('email', value);
+        .custom(async value=>{
+            let userToLogin = await db.User.findOne({where:{email: value}});
             // Si el usuario exite devulve usuario, si no devuelve mensaje de error
             if(userToLogin){
                 return true;
             } else {
-                throw new Error('No existe este email.')
+                throw new Error('Ingrese datos validos.')
             }
         }),
     body('password')
         .notEmpty().withMessage('Ingrese su contraseña').bail()
         .isLength({min:8}).withMessage('La contraseña  debe tener al menos 8 caracteres').bail()
-        .custom((value, {req})=>{
-            let userToLogin = findByField('email', req.body.email);
+        .custom(async (value, {req})=>{
+            let userToLogin = await db.User.findOne({where:{email: req.body.email}});
+            if(userToLogin == null){
+                return false
+            }
+            console.log(userToLogin);
             let okPassword = bcrypt.compareSync(value, userToLogin.password);
             //Comparo contraseña hasheada
                 if(okPassword){
