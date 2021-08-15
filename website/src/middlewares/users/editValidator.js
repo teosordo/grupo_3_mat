3ae1cc992fce420506438069edbbbad31992794e
AngieDutra <angiedutraa@gmail.com>
2021-08-15 17:14:41 -0300

    Visualizacion de errores de validacion

--- website/src/middlewares/users/editValidator.js
+++ website/src/middlewares/users/editValidator.js
@@ -8,12 +8,14 @@ module.exports = [
         .notEmpty().withMessage('Ingrese su nuevo e-mail').bail()
         .isEmail().withMessage('Ingrese un e-mail válido')
         .custom(async (value, {req}) =>{
-            /* Revisa si hay un usuario con el mismo email*/
             try {
                 let user = await db.User.findByPk(req.params.id)
                 let userEmail = await db.User.findOne({where:{email: value}})
-                if(userEmail && userEmail==user.email){
-                    throw new Error('Este e-mail se encuentra en uso')
+                // Si hay un usuario con ese mail
+                // Y si el que figura excistente no pertenece al user que se está editando
+                // Devuelve error
+                if(userEmail && userEmail.id != user.id){
+                    throw new Error('Ese e-mail no se encuentra disponible')
                 }else{
                     return true
                 }  
@@ -23,14 +25,17 @@ module.exports = [
         }), 
     body('username')
         .notEmpty().withMessage('Ingrese un nuevo nombre de usuario').bail()
-        .isLength({min: 5}).withMessage('El usuario debe tener al menos 5 caracteres').bail()
+        .isLength({min: 5}).withMessage('El nombre de usuario debe tener al menos 5 caracteres').bail()
         .custom(async (value, {req}) =>{
             /* Revisa si hay un usuario con el username*/
             try {
                 let user = await db.User.findByPk(req.params.id)
                 let userName = await db.User.findOne({where:{username: value}})
-                if(userName != null && userName==user.username){
-                    throw new Error('El nombre de usuario se encuentra en uso')
+                // Si hay un usuario con ese nombre de usuario
+                // Y si el que figura excistente no pertenece al user que se está editando
+                // Devuelve error
+                if(userName != null && userName.id != user.id){
+                    throw new Error('Ese nombre de usuario no se encuentra disponible')
                 }else{
                     return true
                 }
@@ -43,7 +48,7 @@ module.exports = [
             // Solo si la contraseña tiene valores entre 1 y 8 aparece un arror de caracteres minimos
             // Si se deja vacio se conserva la contraseña del usuario
             if(value.length>0 && value.length <8){
-                throw new Error('El nombre de usuario se encuentra en uso')
+                throw new Error('La contraseña debe tener al menos 8 caracteres')
             } else {
                 return true
             }
