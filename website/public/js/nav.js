2af773c6092c7d868090827f2752f021ecc72557
AngieDutra <angiedutraa@gmail.com>
2021-09-17 20:25:01 -0300

    Menu funcional y updates css

--- website/public/js/nav.js
+++ website/public/js/nav.js
@@ -1,8 +1,9 @@
 window.onload = () => {
     const iconMenu = document.querySelector('.burger');
+    const linkIcono = document.querySelector('#linkBurguer');
     const hamburguerMenu = document.querySelector('#navbar');
-
-    iconMenu.addEventListener('mouseover', (e)=>{
+    
+    iconMenu.addEventListener('click', (e)=>{
         //cambios de estilos
         hamburguerMenu.classList.toggle('activeMenu');
     })
