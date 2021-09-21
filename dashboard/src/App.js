292fa3ee7f587a9a150ef6195b228e53d36e6a85
teosordo <teosordo98@gmail.com>
2021-09-20 22:59:55 -0300

    rutas del dashboard creadas

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,10 +1,33 @@
 import './App.css';
+import {Link, Route, Switch} from 'react-router-dom';
+import Home from './components/Home/Home'
 import TotalCountRow from './components/TotalCountRow/TotalCountRow';
+import LastUserCreated from './components/LastUserCreated/LastUserCreated'
 
 function App() {
   return (
     <div className="App">
-      <TotalCountRow/>
+      <Link to='/'>Home</Link>
+      <br/>
+      <Link to='/total-count-row'>Totales</Link>
+      <br/>
+      <Link to='/last-user-created'>Último usuario creado</Link>
+      <br/>
+      <Link to='/last-product-created'>Último producto creado</Link>
+      <br/>
+      <Link to='/categories'>Categorías de productos</Link>
+      <br/>
+      <Link to='/products-list'>Listado de productos</Link>
+      <br/>
+
+      <Switch>
+        <Route exact path='/' component={Home}/>
+        <Route path='/total-count-row' component={TotalCountRow}/>
+        <Route path='/last-user-created' component={LastUserCreated}/>
+        {/* <Route path='/last-product-created' component={LastProductCreated}/>
+        <Route path='/categories' component={}/>
+        <Route path='/products-list' component={}/> */}
+      </Switch>
     </div>
   );
 }
---
+++ dashboard/src/components/LastUserCreated/LastUserCreated.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {useState, useEffect} from 'react';
+
+function LastUserCreated(){
+    return(
+        <div>
+            <h2>Último usuario creado</h2>
+        </div>
+    )
+}
+
+export default LastUserCreated;
