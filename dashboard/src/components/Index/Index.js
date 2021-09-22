5579a3f6fba93804905454463f5306fe2a69ecb4
AngieDutra <angiedutraa@gmail.com>
2021-09-22 16:48:15 -0300

    Index de dashboard

---
+++ dashboard/src/components/Index/Index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Index(){
+    return(
+        <article>
+            <h2>¡Hola!</h2>
+            <h3>Te damos la bienvenida a nuestro dashboard :)</h3>
+            <p>Aquí encontraras la información de concerniente a MATech. Esperamos encuentres lo que estás buscando.</p>
+        </article>
+    )
+}
+
+export default Index;
