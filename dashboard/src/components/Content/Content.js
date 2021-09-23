e828c20a3fcb7472029bea9e090f78b2573ffc90
matiasc289 <matiascelentano562@gmail.com>
2021-09-23 12:03:43 -0300

    cambio home de dashboard

--- dashboard/src/components/Content/Content.js
+++ dashboard/src/components/Content/Content.js
@@ -17,7 +17,13 @@ function Content(){
         <div className="Content">
             <h1>Información de MATech</h1>
             <Switch>
-                <Route exact path='/' component={Index}/>
+                <Route exact path='/'>
+                    <TotalCountRow/>
+                    <LastUserCreated/>
+                    <CategoriesList/>
+                    <ProductList/>
+                    <LastProductCreated/>
+                </Route>
                 <Route path='/total-count-row' component={TotalCountRow}/>
                 <Route path='/last-user-created' component={LastUserCreated}/>
                 <Route path='/categories' component={CategoriesList}/>
