29e6164e25afb281b3b44c92df8a84fe77a1ee09
matiasc289 <matiascelentano562@gmail.com>
2021-09-23 13:20:45 -0300

    fix brand edit error validacion

--- website/src/middlewares/products/brandEdit.js
+++ website/src/middlewares/products/brandEdit.js
@@ -2,5 +2,17 @@ const {body} = require('express-validator');
 const db = require('../../database/models');
 module.exports = [
     body('name')
-        .notEmpty().withMessage('Ingrese el nombre de la marca'),
+        .notEmpty().withMessage('Ingrese el nombre de la marca')
+        .custom(async value =>{
+            try {
+                let brandName = await db.Brand.findOne({where:{name: value}})
+                if(brandName) {
+                    throw new Error('Nombre en uso')
+                }else{
+                    return true
+                }
+            } catch (error) {
+                throw error
+            }
+        }),
 ]
